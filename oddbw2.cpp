e958b4ff55b5bd372fda3bcbb4b49727f49b2c29
pkpranav28 <50529846+pkpranav28@users.noreply.github.com>
2019-05-13 21:29:44 +0530

    Add files via upload

---
+++ oddbw2.cpp
@@ -0,0 +1,20 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+	int s,e,i;
+	cout<<"Enter a starting number:";
+	cin>>s;
+	cout<<"Enter the ending number:";
+	cin>>e;
+	if(s!=e)
+	{
+	for(i=++s;i<e;i++)
+	{
+		if(i%2!=0)
+		cout<<"\t"<<i;
+	}
+	}
+	else
+	cout<<"Invalid";
+}
