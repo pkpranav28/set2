3968d915b642f6a52245a3b741970b46aea12dbf
pkpranav28 <50529846+pkpranav28@users.noreply.github.com>
2019-05-13 21:19:22 +0530

    Add files via upload

---
+++ palindrome.cpp
@@ -0,0 +1,25 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+	int n,t,a,rev=0;
+	cout<<"Enter a value:";
+	cin>>n;
+	if(n>0)
+	{
+	t=n;
+	do
+	{
+		a=n%10;
+		rev=(rev*10)+a;
+		n=n/10;
+	}while(n!=0);
+	if(t==rev)
+	cout<<t<<" is a Palindrome";
+	else
+	cout<<t<<" is not a Palindrome";
+	}
+	else
+	cout<<"Invalid";
+}
+	
